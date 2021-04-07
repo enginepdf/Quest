be28b15ddbb72f4d1b51d735106d1468b95473fc
enginepdf <62423408+enginepdf@users.noreply.github.com>
2021-04-07 22:55:57 +0900

    Update roles.tf
    roles tf comment 제거

--- Infra/roles.tf
+++ Infra/roles.tf
@@ -13,14 +13,12 @@ data "aws_iam_policy_document" "ecs_task_execution_role" {
 
 }
 
-
 resource "aws_iam_role" "ecs_task_execution_role" {
   name               = "ECSTaskExecutionRole"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
 
 }
 
-# ECS task execution role policy attachment
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
