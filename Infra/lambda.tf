30af0d23c791b97bf92fd99a52a0d9374feadebc
enginepdf <62423408+enginepdf@users.noreply.github.com>
2021-04-08 01:25:37 +0900

    CREATE lambda.zip
    lambda.zip 업로드 및 핸들러 명칭을 lambda.tf에서 수정

--- Infra/lambda.tf
+++ Infra/lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "lambda_tf" {
   function_name = "lambda_handler"
   role          = "${aws_iam_role.iam_for_lambda.arn}"
 
-  handler       = "lambda.lambda_handler"  // filename is lambda
+  handler       = "index.handler"  // filename is index
   runtime       = "nodejs14.16.1"
   
   source_code_hash = "${filebase64sha256("lambda.zip")}"
