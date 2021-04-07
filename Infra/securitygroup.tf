3eea5f8606869ac336043e7f0aa2231d079f54f3
enginepdf <62423408+enginepdf@users.noreply.github.com>
2021-04-07 22:53:48 +0900

    Create securitygroup.tf
    security group tf code 작성

---
+++ Infra/securitygroup.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "alb" {
+  name        = "ALB-security-group"
+  description = ""
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 3000
+    to_port     = 3000
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+      Name = "ALB"
+  }
+}
+
+resource "aws_security_group" "ecs_tasks" {
+  name        = "tasks-security-group"
+  description = "allow inbound access from the ALB only"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = 3000
+    to_port         = 3000
+    security_groups = [aws_security_group.alb.id]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+      Name = "ecs"
+  }
+}
