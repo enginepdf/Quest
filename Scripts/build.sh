2b651decacdcdf36e2ae0ac65f79335fa1936d52
enginepdf <62423408+enginepdf@users.noreply.github.com>
2021-04-08 23:35:13 +0900

    Update build.sh
    Scripts --> scripts   cd 명령어 추가

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,8 +1,12 @@
 # Server build
+
+cd Server
 docker build -t $DOCKER_ID/$IMAGE .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_ID" --password-stdin
 docker push $DOCKER_ID/$IMAGE
 docker rmi $DOCKER_ID/$IMAGE
+cd ..
 
 # ClientQ build
-npx lerna run build
+
+npx lerna run build # cd ClientQ; npm run build
