7a26da1cde6dd1d5c99cd8501d705d36b62b752e
enginepdf <yeindap@gmail.com>
2021-04-10 00:15:01 +0900

    Update variables.tf
    type and default 추가

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -1,17 +1,29 @@
 variable "region" {
+    type = string
+    default = ""
 }
 
 variable "access_key" {
+    type = string
+    default = ""
 }
 
 variable "secret_key" {
+    type = string
+    default = ""
 }
 
 variable "crtARN" {
+    type = string
+    default = ""
 }
 
 variable "app_image" {
+    type = string
+    default = ""
 }
 
 variable "bucket" {
+    type = string
+    default = ""
 }
