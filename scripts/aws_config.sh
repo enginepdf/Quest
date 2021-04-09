a662999b8a83bc50ae053022f6d7e929119a6d33
enginepdf <yeindap@gmail.com>
2021-04-10 00:36:09 +0900

    Create aws_config.sh
    aws configure profile 관련 .sh 작성

---
+++ scripts/aws_config.sh
@@ -0,0 +1,5 @@
+# aws configure
+aws configure set profile.quest.region $REGION
+aws configure set profile.quest.aws_access_key_id $ACCESS_KEY  
+aws configure set profile.quest.aws_secret_access_key $SECRET_KEY
+aws configure --profile quest
