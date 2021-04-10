de6e18d5cd73752b229e02946a30fcb738ce10cd
enginepdf <yeindap@gmail.com>
2021-04-10 22:50:03 +0900

    Update test.sh
    docker stop과  관련된 구문 수정

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,5 +5,5 @@ docker run -d -p 3000:3000 $DOCKER_ID/$IMAGE
 npm run test
 
 kill -9 `lsof -i:5000 | grep "node"|awk '{print $2}'`
-docker stop docker run -d -p 3000:3000 $DOCKER_ID/$IMAGE
+docker stop $DOCKER_ID/$IMAGE
 exit;
