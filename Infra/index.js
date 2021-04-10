9c0659bca0a8945b2c8871a0ba1afae76efcf399
enginepdf <yeindap@gmail.com>
2021-04-10 23:43:32 +0900

    UPDATE Quest
    Infra의 lambda 함수 관련 코드들도 linting 처리를 위해 관련 package.json 수정

---
+++ Infra/index.js
@@ -0,0 +1,7 @@
+exports.handler = async () => { // (event)=>
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('This is /check2 from Lambda!'),
+    };
+    return response;
+};
