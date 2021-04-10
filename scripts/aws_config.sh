57a0874422d30ad7c9d34a0830f342d7742ae921
enginepdf <yeindap@gmail.com>
2021-04-10 22:30:33 +0900

    Update aws_config.sh
    exit; 추가

--- scripts/aws_config.sh
+++ scripts/aws_config.sh
@@ -3,3 +3,5 @@ aws configure set profile.quest.region $REGION
 aws configure set profile.quest.aws_access_key_id $ACCESS_KEY  
 aws configure set profile.quest.aws_secret_access_key $SECRET_KEY
 aws configure --profile quest
+
+exit;
