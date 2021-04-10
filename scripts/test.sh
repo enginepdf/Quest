0d4391bc2bcac1d99e5e854fa8a4bf521fcd6384
enginepdf <yeindap@gmail.com>
2021-04-10 22:32:56 +0900

    Create test.sh
    빌드 후의 테스트 진행에 관한 .sh 추가

---
+++ scripts/test.sh
@@ -0,0 +1,9 @@
+serve -s ClientQ/build -l 5000 &
+echo -ne '\n'
+
+docker run -d -p 3000:3000 $DOCKER_ID/$IMAGE
+npm run test
+
+kill -9 `lsof -i:5000 | grep "node"|awk '{print $2}'`
+docker stop docker run -d -p 3000:3000 $DOCKER_ID/$IMAGE
+exit;
