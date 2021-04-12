3b406b33a84210da0dedc928c5982ea2adc00be1
enginepdf <yeindap@gmail.com>
2021-04-12 17:20:15 +0900

    UPDATE Quest/scripts
    AWS Codebuild는 root로 빌드를 한다는 점을 이용하여 sudo 구문 사용과 .tfvars 파일에 환경변수를 넣는 구문 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,13 @@
 # cd Infra
-# terraform plan
-# terraform apply
+echo "region=\"$REGION\"" >> terraform.tfvars
+echo "access_key=\"$ACCESS_KEY\"" >> terraform.tfvars
+echo "secret_key=\"$SECRET_KEY\"" >> terraform.tfvars
+echo "crtARN=\"$crtARN\"" >> terraform.tfvars
+echo "app_image=\"$app_image\"" >> terraform.tfvars
+echo "bucket=\"$BUCKET_NAME\"" >> terraform.tfvars
+
+# terraform init -input=false
+# terraform apply -input=false -auto-approve
 
 aws s3 rm s3://$BUCKET_NAME/ --recursive
 aws s3 cp ClientQ/build s3://$BUCKET_NAME --recursive
--- scripts/install.sh
+++ scripts/install.sh
@@ -7,9 +7,9 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
-npm install serve -g  # to start client server with ClientQ/build
-
-sudo npm install pm2 -g #  to start servers on background
-
+sudo npm install serve pm2 -g #  to start servers on background
 
 exit;
+
+# https://docs.aws.amazon.com/codebuild/latest/userguide/troubleshooting.html#troubleshooting-root-build-commands
+# By default, CodeBuild runs all build commands as the root user
