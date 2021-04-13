65120b805151b3c4eeeb4e9c553471a66ebce02c
enginepdf <yeindap@gmail.com>
2021-04-13 21:00:00 +0900

    Update deploy.sh
    terraform.tfvars 작성 관련 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,17 @@
-# cd Infra
-echo "region=\"$REGION\"" >> terraform.tfvars
+cd Infra
+
+echo "region=\"$REGION\"" >> terraform.tfvars   # terraform automatically loads terraform.tfvars.  >> is for appending, > is for stdout
 echo "access_key=\"$ACCESS_KEY\"" >> terraform.tfvars
 echo "secret_key=\"$SECRET_KEY\"" >> terraform.tfvars
 echo "crtARN=\"$crtARN\"" >> terraform.tfvars
-echo "app_image=\"$app_image\"" >> terraform.tfvars
+echo "app_image=\"$app_image\"" >> terraform.tfvars  # echo "app_image=\"$DOCKER_ID/$IMAGE\":latest" >> terraform.tfvars
 echo "bucket=\"$BUCKET_NAME\"" >> terraform.tfvars
 
 # terraform init -input=false
 # terraform apply -input=false -auto-approve
+cd ..
+
+# sleep 1
 
 aws s3 rm s3://$BUCKET_NAME/ --recursive
 aws s3 cp ClientQ/build s3://$BUCKET_NAME --recursive
